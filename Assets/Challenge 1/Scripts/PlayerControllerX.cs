bf455cc1a540da2d57b121b2b2e64d2119450b6c
Artem <gpsqaz1@gmail.com>
2024-04-03 16:26:44 +0700

    Initial commit

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float rotationSpeed = 55.0f;
+    private float verticalInput;
+
+    void Update()
+    {    
+        verticalInput = Input.GetAxis("Vertical"); 
+       
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.left, Time.deltaTime * rotationSpeed * verticalInput);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float turnSpeed = 25.0f;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    public Camera mainCamera;
+    public Camera driverCamera;
+    public KeyCode switchKey;
+
+    public string inputID;
+
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+
+        //move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            driverCamera.enabled = !driverCamera.enabled;
+        }
+
+    }
+}
+                                             
