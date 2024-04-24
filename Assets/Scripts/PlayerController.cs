b14c6fc291a1f3af665c07e8202f1c7f1a177b6b
Artem <gpsqaz1@gmail.com>
2024-04-24 19:43:38 +0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]private float horsePower = 0f;
+    [SerializeField] private float horsePower = 0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
-    private float forwardInput;
-
+    private float verticalInput;
     private Rigidbody playerRb;
 
     /* public Camera mainCamera;
@@ -25,11 +24,11 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
 
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal1");
+        verticalInput = Input.GetAxis("Vertical1");
 
         //move the vehicle forward
-        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
         /*if (Input.GetKeyDown(switchKey))
