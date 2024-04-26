f290a8869b6d95b977e4597a461698e1e690b9af
Artem <gpsqaz1@gmail.com>
2024-04-26 13:46:30 +0700

    Update PlayerController.cs
    добавлена проверка условия на соприкосновение колёс с землей, чтобы во время падения машины невозможно было ей управлять

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float horsePower = 0f;
     [SerializeField] float speed;
     [SerializeField] float rpm;
+
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody playerRb;
+    int wheelsOnGround;
 
     [SerializeField] TextMeshProUGUI speedometerText;
     [SerializeField] TextMeshProUGUI rpmText;  
     [SerializeField] GameObject centerOfMass;
+    [SerializeField] List<WheelCollider> allWheels;
 
     /* public Camera mainCamera;
      public Camera driverCamera;
@@ -32,27 +35,49 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-
-        horizontalInput = Input.GetAxis("Horizontal1");
-        verticalInput = Input.GetAxis("Vertical1");
-
-        //move the vehicle forward
-        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
-
-        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f);
-        speedometerText.SetText("Score: " + speed + " km/h");
-
-        rpm = Mathf.Round((speed % 30) * 40);
-        rpmText.SetText("RPM: " + rpm);
-
-
-        /*if (Input.GetKeyDown(switchKey))
+        if (IsOnGround())
         {
-            mainCamera.enabled = !mainCamera.enabled;
-            driverCamera.enabled = !driverCamera.enabled;
-        }*/
+            horizontalInput = Input.GetAxis("Horizontal1");
+            verticalInput = Input.GetAxis("Vertical1");
 
+            //move the vehicle forward
+            playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f);
+            speedometerText.SetText("Score: " + speed + " km/h");
+
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+
+
+            /*if (Input.GetKeyDown(switchKey))
+            {
+                mainCamera.enabled = !mainCamera.enabled;
+                driverCamera.enabled = !driverCamera.enabled;
+            }*/
+        }
+    }
+
+
+    bool IsOnGround()
+    {
+        foreach(WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)  //атрибут isGrounded постоянно обновляется и проверяет находятся ли "колёса" на земле
+            {
+                wheelsOnGround++;
+            }          
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
                                              
